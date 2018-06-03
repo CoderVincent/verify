5f6486fe032f8762af88c5665c458094c6ea282b
superbee <793893252@qq.com>
2018-06-03 17:27:39 +0800

    Update AppConstant.java

--- src/main/java/cn/bookcycle/panda/verify/constant/AppConstant.java
+++ src/main/java/cn/bookcycle/panda/verify/constant/AppConstant.java
@@ -1,7 +1,7 @@
 package cn.bookcycle.panda.verify.constant;
 
 public class AppConstant {
-    public final static int appid = "填写你所申请到的appid";
-    public final static String appkey = "填写你所申请到的appkey";
-    public final static int templateId = 97880;
+//     public final static int appid = "填写你所申请到的appid";
+//     public final static String appkey = "填写你所申请到的appkey";
+//     public final static int templateId = 97880;
 }
