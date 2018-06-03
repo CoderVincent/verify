c228c17a1a920acb75caf9ed0a2949e3a6ad4db5
superbee <793893252@qq.com>
2018-06-03 17:20:29 +0800

    Update testController.java

--- src/main/java/cn/bookcycle/panda/verify/controller/testController.java
+++ src/main/java/cn/bookcycle/panda/verify/controller/testController.java
@@ -12,18 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class testController {
 
-    @RequestMapping(value = "/message",method = RequestMethod.POST)
-    public String test(@RequestParam(value = "verifyCode") Integer verifyCode,
-                        @RequestParam(value = "phoneNumber") String phoneNumber){
-        SmsSingleSenderResult result = new SmsSingleSenderResult();
-        try{
-            SmsSingleSender sender=new SmsSingleSender(AppConstant.appid,AppConstant.appkey);
-            String[] params = {verifyCode.toString()};
-            result = sender.sendWithParam("86",phoneNumber, AppConstant.templateId,params,"","","");
-            System.out.print(result);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return result.toString();
-    }
+//     @RequestMapping(value = "/message",method = RequestMethod.POST)
+//     public String test(@RequestParam(value = "verifyCode") Integer verifyCode,
+//                         @RequestParam(value = "phoneNumber") String phoneNumber){
+//         SmsSingleSenderResult result = new SmsSingleSenderResult();
+//         try{
+//             SmsSingleSender sender=new SmsSingleSender(AppConstant.appid,AppConstant.appkey);
+//             String[] params = {verifyCode.toString()};
+//             result = sender.sendWithParam("86",phoneNumber, AppConstant.templateId,params,"","","");
+//             System.out.print(result);
+//         }catch (Exception e){
+//             e.printStackTrace();
+//         }
+//         return result.toString();
+//     }
 }
