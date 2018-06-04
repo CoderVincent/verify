02b7549a7492a2e27c67c989f6206ab52ea3933d
superbee <793893252@qq.com>
2018-06-04 20:51:34 +0800

    Update messageController.java

--- src/main/java/cn/bookcycle/panda/verify/controller/messageController.java
+++ src/main/java/cn/bookcycle/panda/verify/controller/messageController.java
@@ -3,7 +3,7 @@ package cn.bookcycle.panda.verify.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class messageController {
     @RequestMapping(value = "/login")
     public String verifycode(){
